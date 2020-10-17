6ec6bedbb1da08a126b94989912906abc610bf3b
Dmitry Mavlin <mavlind@list.ru>
2020-10-17 10:41:07 +0700

    added: add prettier, setup .prettierrc.yml

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,11 +7,18 @@ module.exports = {
     'plugin:vue/essential',
     '@vue/standard'
   ],
+  // plugins: [
+      // 'babel',
+      // 'import',
+      // 'prettier',
+  // ],
   parserOptions: {
     parser: 'babel-eslint'
   },
-  rules: {
+  rules: { /* override/add rules from https://eslint.vuejs.org/rules/ */
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'warn',
+    'comma-dangle': 'off',
   }
 }
