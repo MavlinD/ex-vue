6d4217d57a87b4ac5e8e0702c0fe248b0d1c6d4b
Dmitry Mavlin <mavlind@list.ru>
2020-10-30 20:30:56 +0700

    added: e2e test for auth

---
+++ tests/e2e/specs/test-auth.js
@@ -0,0 +1,22 @@
+// https://docs.cypress.io/api/introduction/api.html
+
+describe('test auth user', () => {
+  it('wrong auth', () => {
+    const fakeId = 'fake id' // 5f8475902b0be670555f1bb3
+    const siteId = '5f8475902b0be670555f1bb3'
+    cy.viewport(1200, 1100)
+    cy.visit('/analytics')
+    cy.url().should('include', '/sign-in')
+    cy.get('form')
+    cy.get('input[name="site-id"]').type(fakeId).should('have.value', fakeId)
+    cy.get('form').submit()
+    cy.get('.err-msg')
+    cy.get('button').and('be.disabled')
+    cy.get('input[name="site-id"]').clear()
+    cy.get('input[name="site-id"]').type(siteId).should('have.value', siteId)
+    cy.wait(1500)
+    cy.get('.err-msg').and('be.empty')
+    cy.get('button').and('not.be.disabled')
+    cy.get('form').submit()
+  })
+})
